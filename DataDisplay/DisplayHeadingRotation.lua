4efc39e1f427fb6cfe45a9308f30f6d7e2a9be44
RandomModeller <71475850+RandomModeller@users.noreply.github.com>
2026-07-31 23:48:14 +0700

    Update DisplayHeadingRotation.lua --> v1.1.0
    Add multiplier

--- DataDisplay/DisplayHeadingRotation.lua
+++ DataDisplay/DisplayHeadingRotation.lua
@@ -1,8 +1,17 @@
-behaviour("DisplayHeadingRotation") --v1.0.0
+behaviour("DisplayHeadingRotation") --v1.1.0
 
 function DisplayHeadingRotation:Start()
+    self.dataCon = self.gameObject.GetComponent(DataContainer)
+
     self.vehicleObject = self.targets.vehicleObject.transform
     self.transform = self.gameObject.transform
+
+    self.multiplier = Vector3.zero
+    if self.dataCon then
+        if self.dataCon.HasVector("multiplier") then
+            self.multiplier = self.dataCon.GetVector("multiplier")
+        end
+    end
 
     self.display = false
 end
@@ -14,5 +23,5 @@ function DisplayHeadingRotation:Update()
         return
     end
 
-    self.transform.localEulerAngles = Vector3(0, 0, self.vehicleObject.eulerAngles.y)
+    self.transform.localEulerAngles = self.vehicleObject.eulerAngles.y * self.multiplier
 end
